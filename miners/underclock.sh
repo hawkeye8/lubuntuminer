79625da347d831f124a8f7fdb46e1a9fe0247531
hawkeye8 <34776856+hawkeye8@users.noreply.github.com>
2017-12-22 09:53:35 -1000

    Update underclock.sh

--- miners/underclock.sh
+++ miners/underclock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=80
-sudo nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=80
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 90
-#sudo nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=600
-#sudo nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=600
+sudo nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=80
+sudo nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=80
+#sudo nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=1000
+#sudo nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=1000
