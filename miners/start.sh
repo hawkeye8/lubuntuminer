375ed0926d7568491985583182ff863d160f9f54
hawkeye8 <34776856+hawkeye8@users.noreply.github.com>
2017-12-22 19:01:47 +0000

    Add files via upload

---
+++ miners/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v10.2 - LINUX"
+./ethdcrminer64 -epool us-east.ethash-hub.miningpoolhub.com:17020 -ewal jtoma.1060rig -eworker jtoma.1060rig -esm 2 -epsw x -allcoins 1
